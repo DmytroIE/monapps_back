03e7e76cc1e9d5debdb09f18bf12f90e88287d1b
Dmytro Iermachenko <dmytro.iermachenko@gmail.com>
2024-07-01 16:11:55 +0200

    is_totalizer added

--- monapps/apps/datafeeds/models.py
+++ monapps/apps/datafeeds/models.py
@@ -9,6 +9,7 @@ class DatafeedType(models.Model):
 
     name = models.CharField(max_length=200, blank = False, unique = True)
     base_unit = models.ForeignKey('datafeeds.DfMeasUnit', on_delete=models.SET_NULL, null=True, blank=True)
+    is_totalizer = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -19,10 +20,11 @@ class DfMeasUnit(models.Model):
 
     name = models.CharField(max_length=200, blank = False, unique = True)
     type = models.ForeignKey(DatafeedType, on_delete = models.CASCADE)
-    conv_factor = models.FloatField(default=1.0) # used for conversion to the base unit
+    coeff = models.FloatField(default=1.0) # used as k in k*x+b for conversion to the base unit
+    adder = models.FloatField(default=0.0) # used as b in k*x+b for conversion to the base unit
 
     def to_base_unit(self):
-        return self.type.base_unit*self.conv_factor
+        return self.type.base_unit*self.coeff + self.adder
 
 
     def __str__(self):
